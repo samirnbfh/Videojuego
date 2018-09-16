20f27a449dfd011a02983022bdfa78e43edf3ba1
MUDCROSS1507 <43319453+MUDCROSS1507@users.noreply.github.com>
2018-09-16 11:43:15 -0500

    Update Heroes.java

--- Videojuego/src/Heroes.java
+++ Videojuego/src/Heroes.java
@@ -1,16 +1,19 @@
+/*
+Hecho por Paco C
+15/07/2018
+Clase hija de Personajes en la cual se heredan todos los atributos y solo se ponen los constructores
+*/
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class Heroes extends Personajes {
 	
 
-		public Heroes() {
+		public Heroes() {//Constructor por default
 			super();
-		}
-		public Heroes(String nombre, int x, int y, int HP, int damage, int tam,int tam1) {
+		}//fin del constructor por default
+		public Heroes(String nombre, int HP, int damage, int tam,int tam1) {//construcotr por parametros que recibe el nombre del heroes con el daño y su tamaño
 			nombre=this.nombre;
-			x=this.x;
-			y=this.y;
 			Hp=this.Hp;
 			damage=this.damage;
 			tam=this.tam;
