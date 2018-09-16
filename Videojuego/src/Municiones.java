2d98b074349bc85ef0e70f694077f6624bf6e514
samirnbfh <samirnbfh@gmail.com>
2018-09-16 12:54:03 -0500

    Update Municiones.java

--- Videojuego/src/Municiones.java
+++ Videojuego/src/Municiones.java
@@ -1,77 +1,50 @@
+
 /*
 Hecho por Paco C
 15/07/2018
 Clase de Municiones la cual tendra clases hijas dependiendo del tipo de municion que necesite el arma
-con atributos de nombre, daño, y el tamaño
+con atributos daño, y velocidad
+
+	Editado por Samuel Ramirez 9/16/2018
+	
 */
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
 
-import javax.swing.ImageIcon;
+public abstract class Municiones extends Spawnable{
 
-public class Municiones extends Spawnable{
-
-	protected String nombre;
-	protected boolean hit;
-	protected int damage,tamanio,tamanio1;
-	private  Image  imagen;
+	private int damage; // Daño que causa la municione a otros objeto
+	private int velocidad; //Velocidad a la que se mueve la municion
+	
 	public Municiones() {
-		hit=false;
-		damage=0;tamanio=0;tamanio1=0;
-	}
-	public Municiones(int damage, boolean hit, int tamanio, int tamanio1) {
-		hit=this.hit;
-		damage=this.damage;
-		tamanio=this.tamanio;
-		tamanio1=this.tamanio1;
+		super();
+		damage=0;
+		velocidad = 0;
 	}
 	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public boolean isHit() {
-		return hit;
-	}
-	public void setHit(boolean hit) {
-		this.hit = hit;
-	}
+	public Municiones(int damage,int velocidad,int x, int y,int tamanioX, int tamanioY,String nombre, boolean spawned) {
+		super(x,y,tamanioX,tamanioY,nombre,spawned);
+		this.damage = damage;
+		this.velocidad = velocidad;
+	}	
+	
+	//Regresa el daño que causa la municion
 	public int getDamage() {
 		return damage;
 	}
+	
+	//Asigna el daño que causa la municion
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
-	public int getTamanio() {
-		return tamanio;
+
+	//Regresa velocidad a la que se mueve la municion
+	public int getVelocidad() {
+		return velocidad;
 	}
-	public void setTamanio(int tamanio) {
-		this.tamanio = tamanio;
+	
+	//Asigna la velocidad a la bala
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
 	}
-	public int getTamanio1() {
-		return tamanio1;
-	}
-	public void setTamanio1(int tamanio1) {
-		this.tamanio1 = tamanio1;
-	}
-	public Image getImagen() {
-		return imagen;
-	}
-	public void setImagen(Image imagen) {
-		this.imagen = imagen;
-	}
-	public void paint(Graphics g) {
-		/*
-		File archivo=new File(nombre);
-		
-			imagen= (new ImageIcon(nombre)).getImage();
-				g.drawImage(imagen,x,y,tamanio, tamanio1, null);
-		*/
-		g.setColor(Color.BLACK);
-		g.fillOval(x, y, tamanio, tamanio1);
-	}
+
+	
 }
