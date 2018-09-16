3dc7a5834bd561f46778add447b9dded7bb100c5
samirnbfh <samirnbfh@gmail.com>
2018-09-16 11:47:45 -0500

    Update Spawnable.java

--- Videojuego/src/Spawnable.java
+++ Videojuego/src/Spawnable.java
@@ -4,13 +4,16 @@
 	Creado: 9/16/2018
 	
 	Descripcion: Clase padre de todos los objetos que se van a instanciar en el canva durante la ejecucion del juego
+	
 */
+
+import java.awt.Graphics;
 
 public abstract class Spawnable {
 	
-	private int x;
-	private int y;
-	private boolean spawned;
+	private int x; //Posicion de x del objeto que se va a instanciar
+	private int y; //Posicion de y del objeto que se va a instanciar
+	private boolean spawned; //True= el objeto esta instanciado en el canvas, False: el objeto no esta instanciado en el canvas
 	
 	public Spawnable() {
 		x = 0;
@@ -24,38 +27,48 @@ public abstract class Spawnable {
 		this.spawned = spawned;
 	}
 
+	//Regresa la posicion en x del objeto
 	public int getX() {
 		return x;
 	}
-
+	
+	//Asigna una posicion en x al objeto
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	//Regresa una posicion en y al objeto
 	public int getY() {
 		return y;
 	}
 
+	//Asigna una posicion en y al objeto
 	public void setY(int y) {
 		this.y = y;
 	}
-
+	
+	//Regresa si el objeto esta instanciado
 	public boolean isSpawned() {
 		return spawned;
 	}
 
+	//Asigna si el onjeto esta instanciado o no
 	public void setSpawned(boolean spawned) {
 		this.spawned = spawned;
 	}
 
 	@Override
+	//Imprime la informacion del objeto 
 	public String toString() {
 		return "Spawnable [x=" + x + ", y=" + y + ", spawned=" + spawned + "]";
 	}
 
 	@Override
+	//Metodo abstracto que compara el objeto con otro
 	public abstract boolean equals(Object obj);
 	
+	//Metodo que pinta el objeto en el canvas
+	public abstract void paint(Graphics g);
 	
 	
 }
